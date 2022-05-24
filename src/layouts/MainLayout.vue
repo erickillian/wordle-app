49aa3d2c2b67dcfa3372553b30ae58a4579fc9c5
erickillian <erickillian39@gmail.com>
2022-05-23 22:43:34 -0400

    Styling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,24 +1,20 @@
 <template>
-  <v-app id="app">
-    <navbar />
-    <v-main>
-      <v-container fill-height fluid>
-          <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+    <v-app id="app">
+        <navbar />
+        <v-main>
+            <router-view /> 
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
 
 export default {
-  name: "main-layout",
-  components: { Navbar },
-  data() {
-    return {
-
+    name: "main-layout",
+    components: { Navbar },
+    data() {
+        return {}
     }
-  }
 };
 </script>
